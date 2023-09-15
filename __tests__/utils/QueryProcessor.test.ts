26fa5a7b03ae547751aa037b8583d7c262b9aaa0
jonathanlindstrom1 <jmlindst@andrew.cmu.edu>
2023-09-15 12:24:38 -0400

    Adding in functionality for andrew ID and Testing it

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,13 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return Jmlindst', () => {
+        const query = "andrew";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "Jmlindst"
+          ));
+    });
 });
+
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,12 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("andrew")) {
+    return (
+      "Jmlindst"
+    );
+  }
+
+
   return "";
 }
